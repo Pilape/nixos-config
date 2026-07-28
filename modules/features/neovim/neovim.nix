6a6c1b6293b5326480b6de189a0ea469ce87caa7
= <=>
2026-07-28 19:59:06 +0200

    neovim part 4

--- modules/features/neovim/neovim.nix
+++ modules/features/neovim/neovim.nix
@@ -7,16 +7,42 @@
     };
 
     perSystem = { pkgs, lib, ... }: {
-       packages.neovim = pkgs.symlinkJoin {
+        packages.neovim = let
+            packageName = "neovim-plugins";
+
+            startPlugins = [
+                pkgs.vimPlugins.plenary-nvim
+                pkgs.vimPlugins.telescope-nvim
+            ];
+
+            packpath = pkgs.runCommandLocal "packpath" {} ''
+                mkdir -p $out/pack/${packageName}/{start,opt}
+
+                ${
+                    lib.concatMapStringsSep
+                    "\n"
+                    (plugin: "ln -vsfT ${plugin} $out/pack/${packageName}/start/${lib.getName plugin}")
+                    startPlugins
+                }
+                '';
+        in
+        pkgs.symlinkJoin {
 	    name = "nvim";
 	    paths = [pkgs.neovim-unwrapped];
 	    nativeBuildInputs = [pkgs.makeWrapper];
+
 	    postBuild = ''
 		wrapProgram $out/bin/nvim \
 		--add-flags '-u' \
-		--add-flags '${./init.lua}'
+		--add-flags '${./init.lua}' \
+                --add-flags '--cmd' \
+                --add-flags "'set packpath^=${packpath} | set runtimepath^=${packpath}'" \
+                --set-default NVIM_APPNAME spamton-neovim
 	    '';
-	    
+
+            passthru = {
+                inherit packpath;
+            }; 
 	};
     };
 
