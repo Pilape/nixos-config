070940e02ab6b6ce651fa488c5d0462b2d4d62f2
= <=>
2026-08-01 16:29:45 +0200

    neovim part 6

--- modules/features/neovim/init.lua
+++ modules/features/neovim/init.lua
@@ -6,12 +6,19 @@ vim.opt.shiftwidth = 4
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.lsp.enable({
+vim.lsp.config('ccls', {
     cmd = {'ccls'},
     filetypes = {'c', 'h', 'cpp', 'hpp'},
     root_markers = {'.git'},
 })
+vim.lsp.enable('ccls')
 
+vim.lsp.config('nixd', {
+    cmd = {'nixd'},
+    filetypes = {'nix'},
+    root_markers = {'.git', 'flake.nix', 'flake.lock'}
+})
 vim.lsp.enable('nixd')
+
 
 vim.cmd("colorscheme gruvbox")
