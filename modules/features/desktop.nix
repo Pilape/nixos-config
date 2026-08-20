8c3b0dedc74f0572341dceece50de8e4cec86f64
= <=>
2026-08-20 19:14:23 +0200

    Tried and failed (but also didn't) to fix Java UI

--- modules/features/desktop.nix
+++ modules/features/desktop.nix
@@ -1,12 +1,16 @@
 { self, inputs, ... }: {
 
-    flake.nixosModules.desktop = { pkgs, ... }: let 
+    flake.nixosModules.desktop = { pkgs, lib, ... }: let 
         selfpkgs = self.packages."${pkgs.system}";
     in {
 
         imports = [ self.nixosModules.neovim ];
 
 	services.displayManager.ly.enable = true;
+
+        #environment.systemPackages = with pkgs; [
+        #    wmname
+        #];
 
         fonts.packages = with pkgs; [
             hermit
@@ -15,6 +19,14 @@
         programs.sway = {
             enable = true;
             package = selfpkgs.desktop;
+
+            xwayland.enable = true;
+            wrapperFeatures.gtk = true;
+        };
+
+        environment.sessionVariables = rec {
+            QT_QPA_PLATFORM = "wayland";
+            _JAVA_AWT_WM_NOREPARENTING = 1;
         };
 
         users.defaultUserShell = selfpkgs.environment;
