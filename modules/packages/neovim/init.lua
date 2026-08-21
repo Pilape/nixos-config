a3359c6d8aa42f60d31db06a70aff9bf52593bcb
= <=>
2026-08-22 00:04:41 +0200

    Removed neovim plugin.setup thingy that apparently didn't work

--- modules/packages/neovim/init.lua
+++ modules/packages/neovim/init.lua
@@ -13,7 +13,6 @@ vim.cmd.colorscheme("dim")
 
 --== MISC ==--
 require('autoclose').setup()
-require('obsession').setup()
 require('gitsigns').setup()
 require('blink-cmp').setup({
     completion = { documentation = {
